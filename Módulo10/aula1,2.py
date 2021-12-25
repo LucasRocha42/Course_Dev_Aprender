7303ad926f7b1d16a1b458fb23c69c9ef9b428b2
lucasrocha42 <lucasfariascr1@gmail.com>
2021-12-25 11:04:08 -0200

    First

---
+++ Módulo10/aula1,2.py
@@ -0,0 +1,7 @@
+# o que são APIs
+# Mini-sistema para acessar sistema maior, outros sistema conseguem acessar  para terceiros 
+
+
+# Onde encontrar APIs oficiais e não-oficiais 
+# Github
+# APIs não oficial - api unnofficial api
